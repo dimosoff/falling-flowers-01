d84f9b80265377ec2f95ea51474779014fc2c95a
dimosoff <dimmiostr@mail.ru>
2023-12-12 11:31:13 +0300

    89401 - new-year 2023

---
+++ new-year.js
@@ -0,0 +1,16 @@
+const NEW_YEAR_TOYS = document.querySelectorAll(".new-year__toy");
+
+NEW_YEAR_TOYS.forEach(function (toy) {
+  toy.addEventListener("mouseenter", nyToyHoverHandler);
+});
+
+function nyToyHoverHandler(event) {
+  if (event.target.classList.contains("animate")) return;
+
+  event.target.classList.add("animate");
+
+  let t = setTimeout(function () {
+    event.target.classList.remove("animate");
+    clearTimeout(t);
+  }, 2000);
+}
