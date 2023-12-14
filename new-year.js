53d6ccbbc8f10fdd781acb3c6714753d6411c2bb
dimosoff <dimmiostr@mail.ru>
2023-12-14 17:10:53 +0300

    popup is done

--- new-year.js
+++ new-year.js
@@ -1,17 +1,38 @@
 const NEW_YEAR_TOYS = document.querySelectorAll(".new-year__toy");
+const FLOATING_PRESENT = document.querySelector("button[name='get-a-present']");
+const FLOATING_POPUP_CARDS = document.querySelectorAll(".present-popup__card");
 
 NEW_YEAR_TOYS.forEach(function (toy) {
   toy.addEventListener("mouseenter", nyToyHoverHandler);
   toy.addEventListener("touchstart", nyToyHoverHandler);
+  toy.addEventListener("click", function () {
+    FLOATING_PRESENT.click();
+  });
+});
+
+FLOATING_POPUP_CARDS.forEach(function (card) {
+  card.addEventListener("mouseenter", onCardHover);
+  card.addEventListener("touchstart", onCardHover);
+});
+
+FLOATING_PRESENT.addEventListener("click", function () {
+  FLOATING_PRESENT.classList.add("opened");
 });
 
 function nyToyHoverHandler(event) {
-  if (event.target.classList.contains("animate")) return;
+  const target = event.target;
+  if (target.classList.contains("animate")) return;
 
-  event.target.classList.add("animate");
+  target.classList.add("animate");
 
   let t = setTimeout(function () {
-    event.target.classList.remove("animate");
+    target.classList.remove("animate");
     clearTimeout(t);
-  }, 2000);
+  }, 1450);
+}
+
+function onCardHover(event) {
+  const target = event.target;
+
+  target.classList.add("hovered");
 }
